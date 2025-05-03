f310deb322b8d7759114829277e3768ae7d45867
Mohammad Wasil <mohammad.wasil@h-brs.de>
2025-05-03 22:27:35 +0200

    add publication to index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,5 @@ export { getFormattedDate } from './date'
 export { generateToc } from './generateToc'
 export type { TocItem } from './generateToc'
 export { elementHasClass, toggleClass, rootInDarkMode } from './domElement'
+export { sortPublicationFn } from "./publication";
+export type { Publication } from "./publication";
